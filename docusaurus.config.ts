06c6920dd0d00ea1defb7f62248660a1819a19f5
dienesd <112756483+dienesd@users.noreply.github.com>
2025-05-30 18:25:41 +0200

    publish

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+// taktikai komment a pr-hoz
+
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
